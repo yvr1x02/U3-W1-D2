04ead0aae2c3e81140e194989750d8157a6fc6b3
Yuri Impellizzeri <yuriimpellizzeri02@gmail.com>
2024-07-01 17:49:11 +0200

    Update SingleComment.jsx

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,39 +1,33 @@
-import { Button, ListGroup } from 'react-bootstrap'
+import { Button, ListGroup } from "react-bootstrap";
 
 const SingleComment = ({ comment }) => {
   const deleteComment = async (asin) => {
     try {
-      let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
-        {
-          method: 'DELETE',
-          headers: {
-            Authorization: 'Bearer inserisci-qui-il-tuo-token',
-          },
-        }
-      )
+      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
+        method: "DELETE",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZmU4YjdjMjM5YzAwMTUyZjRiNmUiLCJpYXQiOjE3MTk4MzQ1ODgsImV4cCI6MTcyMTA0NDE4OH0.hHarO-w6zdPV4yhQLDKN3cdJeSB2gCx6uUlN1gUzAJ4",
+        },
+      });
       if (response.ok) {
-        alert('La recensione è stata elimata!')
+        alert("La recensione è stata elimata!");
       } else {
-        throw new Error('La recensione non è stata eliminata!')
+        throw new Error("La recensione non è stata eliminata!");
       }
     } catch (error) {
-      alert(error)
+      alert(error);
     }
-  }
+  };
 
   return (
     <ListGroup.Item>
       {comment.comment}
-      <Button
-        variant="danger"
-        className="ms-2"
-        onClick={() => deleteComment(comment._id)}
-      >
+      <Button variant="danger" className="ms-2" onClick={() => deleteComment(comment._id)}>
         Elimina
       </Button>
     </ListGroup.Item>
-  )
-}
+  );
+};
 
-export default SingleComment
+export default SingleComment;
